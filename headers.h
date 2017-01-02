9f45d49be825750f82a4a756077f017b30e9b753
Michael R. Cook <git@mrcook.uk>
2017-01-02 15:53:50 +0100

    Tiny headers update

--- headers.h
+++ headers.h
@@ -5,23 +5,23 @@
 #pragma once
 
 #ifdef _WIN32
-    #define _CRT_SECURE_NO_WARNINGS
-    #define _CRT_NONSTDC_NO_DEPRECATE
-    #define WIN32_LEAN_AND_MEAN
+  #define _CRT_SECURE_NO_WARNINGS
+  #define _CRT_NONSTDC_NO_DEPRECATE
+  #define WIN32_LEAN_AND_MEAN
 
-    #include <windows.h>
-    #include <io.h>
+  #include <windows.h>
+  #include <io.h>
 
 #elif __APPLE__
 
-    #include <unistd.h>
+  #include <unistd.h>
 
 #elif __linux__
 
-    #include <unistd.h>
+  #include <unistd.h>
 
 #else
-#   error "Unknown compiler"
+  error "Unknown compiler"
 #endif
 
 
